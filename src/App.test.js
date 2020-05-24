2c17a74287ddbdca0ea56de5b9546c189ed7e280
Mandip Tharu <mandip.chaudhary@yarshastudio.com>
2020-05-24 17:10:19 +0545

    app test file updated

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react app', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/covid/i);
   expect(linkElement).toBeInTheDocument();
 });
